deeae7fe6329a579d2b03c0cee993d30b578d003
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-05-22 11:33:39 -0700

    an idea of App

---
+++ src/AppTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class AppTest {
+    @Test
+  void testValidDirectoryIsSet(@TempDir File tempDir) throws FileNotFoundException {
+    // Arrange: Prepare the arguments with the temp directory
+    File directory = new File(tempDir, "subfolder");
+    directory.mkdir();
+    String directoryPath = directory.getAbsolutePath();
+    String[] args = {"-nc", "-h", directoryPath};
+
+    // Act: Create TruffulaOptions instance
+    App apps = new App(args);
+
+    // Assert: Check that the root directory is set correctly
+    
+  }
+}
